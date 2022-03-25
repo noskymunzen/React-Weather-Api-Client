2708605b3c9e9ccff3cece8a7412824e2413429d
noskymunzen <munzenmayer@outlook.com>
2022-03-25 13:44:34 -0300

    feat: vite init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="d-flex justify-content-center mt-5">
+        Check your local region weather ⛅
+      </h1>
+    </>
+  );
+}
+
+export default App;
