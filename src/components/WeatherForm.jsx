d47a2be13440665a0e9ba4f126005aa51c7a8f38
noskymunzen <munzenmayer@outlook.com>
2022-03-27 18:56:22 -0300

    style: adds flexbox to form

--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { COUNTRIES } from '../constants/countries';
 
 const WeatherForm = () => {
   return (
     <Form className="d-flex justify-content-center">
-      <Form.Group>
-        <Form.Control type="text" placeholder="Type your city" />
-      </Form.Group>
+      <Row>
+        <Form.Group className="mb-3 px-0">
+          <Form.Control type="text" placeholder="Type your city" />
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Select aria-label="Default select example">
-          <option>Select a country</option>
-          {Object.entries(COUNTRIES).map(([key, value]) => (
-            <option key={key} value={key}>
-              {value}
-            </option>
-          ))}
-        </Form.Select>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+        <Form.Group className="mb-3 px-0">
+          <Form.Select aria-label="Default select example">
+            <option>Select a country</option>
+            {Object.entries(COUNTRIES).map(([key, value]) => (
+              <option key={key} value={key}>
+                {value}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Row>
     </Form>
   );
 };
