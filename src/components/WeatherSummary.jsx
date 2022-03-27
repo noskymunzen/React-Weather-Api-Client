0db87cf979e00ea78f8d8b7b2cbea4c3bef06896
noskymunzen <munzenmayer@outlook.com>
2022-03-27 19:57:08 -0300

    feat: adds weatherSummary component

---
+++ src/components/WeatherSummary.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const WeatherSummary = ({ city, currentTemp, maxTemp, minTemp }) => {
+  return (
+    <>
+      <h2 className="d-flex justify-content-center mt-5 text-black">
+        {city} weather:
+      </h2>
+      <Container className="d-flex justify-content-center mt-3 mb-3 text-black">
+        <Row xs={1} md={1}>
+          <Col className="h2 d-flex justify-content-center mt-3 mb-5">
+            {currentTemp}°
+          </Col>
+          <Col className="h5 d-flex justify-content-center mb-3">
+            Max: {maxTemp}°
+          </Col>
+          <Col className="h5 d-flex justify-content-center mb-3">
+            Min: {minTemp}°
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default WeatherSummary;
