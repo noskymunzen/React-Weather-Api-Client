788f3d837c2f8fafe1bff9a2ebd0efcbd478d772
noskymunzen <munzenmayer@outlook.com>
2022-03-27 19:59:29 -0300

    feat: integrates api payload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherForm from './components/WeatherForm';
+import $weather from './services/weather';
+import { kelvinToFahrenheit } from './helpers/weather';
+import { Spinner } from 'react-bootstrap';
+import WeatherSummary from './components/WeatherSummary';
 
 function App() {
+  const [hasSuccess, setHasSuccess] = useState(false);
+  const [city, setCity] = useState('');
+  const [currentTemp, setCurrentTemp] = useState(0);
+  const [maxTemp, setMaxTemp] = useState(0);
+  const [minTemp, setMinTemp] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async ({ city, country }) => {
+    setLoading(true);
+    try {
+      const response = await $weather.getWeatherByCityCountry(city, country);
+      setCurrentTemp(kelvinToFahrenheit(response.main.temp));
+      setMaxTemp(kelvinToFahrenheit(response.main.temp_max));
+      setMinTemp(kelvinToFahrenheit(response.main.temp_min));
+      setCity(response.name);
+      setHasSuccess(true);
+    } catch (err) {
+      console.error(err);
+      setHasSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <h1 className="d-flex justify-content-center mt-5">
         Check your local region weather ⛅
       </h1>
-      <WeatherForm />
+      {hasSuccess && !loading && (
+        <WeatherSummary
+          city={city}
+          currentTemp={currentTemp}
+          maxTemp={maxTemp}
+          minTemp={minTemp}
+        />
+      )}
+      {loading && (
+        <Spinner className="mb-5" animation="grow" variant="warning" />
+      )}
+
+      <WeatherForm onSubmit={handleSubmit} />
     </>
   );
 }
